b6f7a4ed753310052c2620d738b36dfab1a130ca
Thiago Silva Farias <tfarias@fb.com>
2020-04-12 19:10:39 +0100

    First Commit

---
+++ Servlet/src/com/project/servlet/Servlet.java
@@ -0,0 +1,57 @@
+package com.project.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 
+ * Servlet skeleton code
+ * Servlet implementation class FirstServlet
+ * 
+ * 
+ * 
+ */
+@WebServlet("/Servlet")
+public class Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	public static final String HTML_START="<html><body>";
+	public static final String HTML_END="</body></html>";
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Servlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	//	response.getWriter().append("Served at: ").append(request.getContextPath());
+		PrintWriter out = response.getWriter();
+		Date date = new Date();
+		out.println(HTML_START + "<h2> Well done your Servlet is working!</h2> <br/> "
+				+ "<h3>  Since servlet is a server-side technology, we will need a [web container] that supports Servlet technology,<br/>  "
+				+ "so we will use the Apache Tomcat server <br/> <br/>  "
+				+ "Date="+date +"</h3>"+HTML_END);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
